42c6b3affb732c9b83d5b1d1bf76a2d0be50d876
William Wolf <wwolf@shopkeep.com>
2017-01-30 15:05:24 +0000

    add basic MVP for uploading a dataframe into shiny

---
+++ app.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Rescue Time Estimation"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(
+        inputId = "rescue.time.report",
+        label = "Please upload your Rescue Time report",
+        multiple = FALSE,
+        accept = c("text/csv, text/comma-separated-values", ".csv")
+      ),
+      tags$hr()
+    ),
+    
+    mainPanel(
+      # plotOutput("dirichletPlot")
+      tableOutput("contents")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$contents <- renderTable({
+    report <- input$rescue.time.report
+    
+    if ( is.null(report) )
+      return(NULL)
+    
+    read.csv(report$datapath)
+  })
+}
+
+shinyApp(ui, server)
