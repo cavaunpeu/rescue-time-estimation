a9ff198da8773b59d85128d05dcd634621fb5047
William Wolf <wwolf@shopkeep.com>
2017-02-01 19:08:38 +0000

    add download, upload and code tab to fixedRow header

--- app.R
+++ app.R
@@ -6,21 +6,39 @@ library(shiny)
 
 ui <- fluidPage(
 
-  titlePanel("Rescue Time Estimation"),
+  titlePanel("RescueTime Estimation"),
 
   sidebarLayout(
-    sidebarPanel(
-      fileInput(
-        inputId = "rescue.time.report",
-        label = "Please upload your Rescue Time report",
-        multiple = FALSE,
-        accept = c("text/csv, text/comma-separated-values", ".csv")
+    fixedRow(
+      column(
+        width = 4,
+        h3("Download report"),
+        style = "padding-left:31px",
+        helpText(
+          "Please download your week-level report at the following",
+          tags$a(href="https://www.rescuetime.com/browse/productivity/by/week/for/the/year/of/2016-01-01", "link.")
+        )
       ),
-      tags$hr()
+      column(
+        width = 4,
+        h3("Upload report"),
+        fileInput(
+          inputId = "rescue.time.report",
+          label = NULL,
+          multiple = FALSE,
+          accept = c("text/csv, text/comma-separated-values", ".csv")
+        )
+      ),
+      column(
+        width = 4,
+        h3("Code"),
+        style = "padding-right:31px",
+        helpText("Voilà the", tags$a(href="http://wp.me/p4zXJT-fm", "code"), "and post for this project")
+      )
     ),
 
     mainPanel(
-      plotOutput("plotPanel")
+      plotOutput("plotPanel", width="100%")
     )
   )
 )
