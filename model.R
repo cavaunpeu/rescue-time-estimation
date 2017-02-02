44023e06ef04b98822740013030bcf8c05b8e035
William Wolf <wwolf@shopkeep.com>
2017-02-02 10:32:20 +0000

    cosmetic changes

--- model.R
+++ model.R
@@ -3,7 +3,7 @@ library(rstan)
 buildModel <- function(report, chains = 1, cores = 1, iter = 1000, warmup = 1000) {
   data <- list(very_distracting = report$very_distracting, distracting = report$distracting, neutral = report$neutral, productive = report$productive, very_productive = report$productive, N = nrow(report))
   model <- stan(file = "model.stan",  data = data, chains = chains, cores = cores, iter = iter)
-  return(model)
+  return( model )
 }
 
 simulatePredictions <- function(model) {
